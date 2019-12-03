8e143bc0bd4a0b7cb1a0c13eab6410f04b6766ae
mtouzot <martin.touzot@gmail.com>
2019-12-03 22:23:53 +0100

    Simple python script to solve Monty Hall Problem

---
+++ montyHallProblem.py
@@ -0,0 +1,47 @@
+#-*- coding:utf-8 -*-
+#!/usr/bin/env python3
+
+import argparse
+import random
+from itertools import permutations
+
+""" Compute a solution to the Monty Hall problem
+
+This script computes a the probabilty to win at Let's Make a Deal game show
+switching doors when the host asks you if you want to do so.
+"""
+
+def montyHallProcess(N):
+    # Init the variable
+    i = 0 # counter
+    keep = 0 # you keep the selected door
+    change = 0 # you change the selected door
+
+    # We generate every possibility
+    gifts = [1, 0, 0]; # 1 : win && 0 : loose
+    doorsPermutations = list(permutations(gifts))
+    selectedPermutation = list(random.choice(doorsPermutations))
+
+    while i < N:
+        i+=1
+
+        #Selection of a door in the selected possibility
+        doorIndexSelected = random.randint(0, len(selectedPermutation)-1)
+        doorSelected = selectedPermutation[doorIndexSelected]
+
+        # If we choose the right door, we should keep it
+        if doorSelected == 1:
+            keep += 1 / N
+        else:
+            change += 1 / N
+
+    print("Probability to win if we change of door: {}".format(change))
+    print("Probability to win if we keep of door : {}".format(keep))
+    
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Compute a solution to the Monty Hall problem.')
+    parser.add_argument('iteration', help='''Number of iteration to compute the 
+                        solution. Default is 100,000''', nargs='?', 
+                        default=100000, type=int)
+    args = parser.parse_args()
+    montyHallProcess(args.iteration)
